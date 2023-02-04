ea5ed77f670ecad3d2cfc661355c8a579f97932f
Dino Gradaskic <123075998+DinoGradaskic@users.noreply.github.com>
2023-02-04 12:08:39 +0100

    first commit

---
+++ components/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <a href="https://youtu.be/dQw4w9WgXcQ"><img 
+                src="./images/troll-face.png" 
+                className="header--image"
+                
+            /></a>
+            <h2 className="header--title">Meme Generator</h2>
+        </header>
+    )
+}
